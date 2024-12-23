e2b2870b508b8da4cf4e869298d818438112f0d6
SamLams24 <attiogbesamuel310@gmail.com>
2024-12-23 10:11:56 +0000

    Setting up other folders -tests etc

---
+++ run.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk-21\bin\java.exe" -XX:+ShowCodeDetailsInExceptionMessages -cp "C:\Users\USER\AppData\Roaming\Code\User\workspaceStorage\e18236bdb59829516819b98b5beef4f2\redhat.java\jdt_ws\ProjetSysDistrib_bbdbaff3\bin" test_folder.MultiCast %1
